00a82c17f42b328bbf530d7251241c402844077c
volkangurbuz <volkangurbuz16@gmail.com>
2019-09-23 19:32:36 +0200

    test

--- app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeListViewModel.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeListViewModel.java
@@ -1,33 +1,14 @@
 package com.codingwithmitch.foodrecipes.viewmodels;
 
-
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.support.annotation.NonNull;
 
 public class RecipeListViewModel extends AndroidViewModel {
 
-    private static final String TAG = "RecipeListViewModel";
+  private static final String TAG = "RecipeListViewModel";
 
-    public RecipeListViewModel(@NonNull Application application) {
-        super(application);
-
-    }
-
-
+  public RecipeListViewModel(@NonNull Application application) {
+    super(application);
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
