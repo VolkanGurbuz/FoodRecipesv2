5aef7a19831c50eb0fea51e67b22c656954f40af
volkangurbuz <volkangurbuz16@gmail.com>
2019-09-23 23:23:32 +0200

    added netword bound resource

--- app/src/main/java/com/codingwithmitch/foodrecipes/AppExecutors.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/AppExecutors.java
@@ -1,23 +1,42 @@
 package com.codingwithmitch.foodrecipes;
 
+import android.os.Handler;
+import android.os.Looper;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.logging.LogRecord;
 
 public class AppExecutors {
 
-    private static AppExecutors instance;
+  private static AppExecutors instance;
 
-    public static AppExecutors getInstance(){
-        if(instance == null){
-            instance = new AppExecutors();
-        }
-        return instance;
+  public static AppExecutors getInstance() {
+    if (instance == null) {
+      instance = new AppExecutors();
     }
+    return instance;
+  }
 
-    private final ScheduledExecutorService mNetworkIO = Executors.newScheduledThreadPool(3);
+  public final Executor mDiskIO = Executors.newSingleThreadExecutor();
+  public final Executor mMainThreadExecutor = new MainThreadExecutor();
 
-    public ScheduledExecutorService networkIO(){
-        return mNetworkIO;
+  public Executor getmDiskIO() {
+    return mDiskIO;
+  }
+
+  public Executor getmMainThreadExecutor() {
+    return mMainThreadExecutor;
+  }
+
+  private static class MainThreadExecutor implements Executor {
+
+    private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+    @Override
+    public void execute(Runnable command) {
+      mainThreadHandler.post(command);
     }
+  }
 }
