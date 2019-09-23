9aaac1dee448cef82e41a6af5c8b61316929520d
volkangurbuz <volkangurbuz16@gmail.com>
2019-09-23 19:48:55 +0200

    added display search category

--- app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
@@ -1,95 +1,106 @@
 package com.codingwithmitch.foodrecipes;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-
 
 import com.codingwithmitch.foodrecipes.adapters.OnRecipeListener;
 import com.codingwithmitch.foodrecipes.adapters.RecipeRecyclerAdapter;
 import com.codingwithmitch.foodrecipes.util.VerticalSpacingItemDecorator;
 import com.codingwithmitch.foodrecipes.viewmodels.RecipeListViewModel;
 
-
 public class RecipeListActivity extends BaseActivity implements OnRecipeListener {
 
-    private static final String TAG = "RecipeListActivity";
+  private static final String TAG = "RecipeListActivity";
 
-    private RecipeListViewModel mRecipeListViewModel;
-    private RecyclerView mRecyclerView;
-    private RecipeRecyclerAdapter mAdapter;
-    private SearchView mSearchView;
+  private RecipeListViewModel mRecipeListViewModel;
+  private RecyclerView mRecyclerView;
+  private RecipeRecyclerAdapter mAdapter;
+  private SearchView mSearchView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe_list);
-        mRecyclerView = findViewById(R.id.recipe_list);
-        mSearchView = findViewById(R.id.search_view);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_recipe_list);
+    mRecyclerView = findViewById(R.id.recipe_list);
+    mSearchView = findViewById(R.id.search_view);
 
-        mRecipeListViewModel = ViewModelProviders.of(this).get(RecipeListViewModel.class);
+    mRecipeListViewModel = ViewModelProviders.of(this).get(RecipeListViewModel.class);
 
-        initRecyclerView();
-        initSearchView();
-        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
-    }
+    initRecyclerView();
+    initSearchView();
+    setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+    subscribeObseserver();
+  }
 
+  private void subscribeObseserver() {
 
-    private void initRecyclerView(){
-        mAdapter = new RecipeRecyclerAdapter(this);
-        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(30);
-        mRecyclerView.addItemDecoration(itemDecorator);
-        mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
+    mRecipeListViewModel
+        .getViewState()
+        .observe(
+            this,
+            new Observer<RecipeListViewModel.ViewState>() {
+              @Override
+              public void onChanged(@Nullable RecipeListViewModel.ViewState viewState) {
+                if (viewState != null) {
+                  switch (viewState) {
+                    case RECIPES:
+                      {
+                        // reci[es will show automatically from another observers
+                        break;
+                      }
+                    case CATEGORIES:
+                      {
+                        displaySeachCategories();
+                        break;
+                      }
+                  }
+                }
+              }
+            });
+  }
 
-    private void initSearchView(){
-        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
+  private void displaySeachCategories() {
+    mAdapter.displaySearchCategories();
+  }
 
+  private void initRecyclerView() {
+    mAdapter = new RecipeRecyclerAdapter(this);
+    VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(30);
+    mRecyclerView.addItemDecoration(itemDecorator);
+    mRecyclerView.setAdapter(mAdapter);
+    mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+  }
 
-                return false;
-            }
+  private void initSearchView() {
+    mSearchView.setOnQueryTextListener(
+        new SearchView.OnQueryTextListener() {
+          @Override
+          public boolean onQueryTextSubmit(String s) {
 
-            @Override
-            public boolean onQueryTextChange(String s) {
-                return false;
-            }
+            return false;
+          }
+
+          @Override
+          public boolean onQueryTextChange(String s) {
+            return false;
+          }
         });
-    }
+  }
 
-    @Override
-    public void onRecipeClick(int position) {
-        Intent intent = new Intent(this, RecipeActivity.class);
-        intent.putExtra("recipe", mAdapter.getSelectedRecipe(position));
-        startActivity(intent);
-    }
+  @Override
+  public void onRecipeClick(int position) {
+    Intent intent = new Intent(this, RecipeActivity.class);
+    intent.putExtra("recipe", mAdapter.getSelectedRecipe(position));
+    startActivity(intent);
+  }
 
-    @Override
-    public void onCategoryClick(String category) {
-        
-    }
-
+  @Override
+  public void onCategoryClick(String category) {}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
